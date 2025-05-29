a55a0568435b43b596f84f0b531e494faf86fa07
desy <desypuspita685@gmail.com>
2025-05-29 15:51:37 +0700

    casemethod

---
+++ Kendaraan.java
@@ -0,0 +1,22 @@
+public class Kendaraan {
+    String platNomor;
+    String tipe; 
+    String merek;
+
+    public Kendaraan(String platMomor, String tipe, String merek) {
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merek = merek;
+    }
+
+    public String getPlatNommor() {
+        return platNomor;
+    }
+    public void tampilkanInformasi() {
+        System.out.println("Plat Nomor : " + platNomor);
+        System.out.println("Tipe : " + tipe);
+        System.out.println("Merek: " + merek);
+    }
+
+
+}
